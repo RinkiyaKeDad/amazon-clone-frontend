be004a2abc5d3cc6b57925976d07ca4159363106
RinkiyaKeDad <arshsharma461@gmail.com>
2020-09-14 19:42:40 +0530

    subtotal section made responsive

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Checkout.css';
+import Subtotal from '../Subtotal/Subtotal';
+import { Grid } from '@material-ui/core';
 
 function Checkout() {
   return (
-    <div className='checkout'>
-      <div className='checkout__left'>
+    <Grid container xs={12} className='checkout'>
+      <Grid item xs={12} sm={9} className='checkout__left'>
         <img
           className='checkout__ad'
           src='https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg'
@@ -14,12 +16,12 @@ function Checkout() {
         <div>
           <h2 className='checkout__title'>Your shopping Basket</h2>
         </div>
-      </div>
+      </Grid>
 
-      <div className='checkout__right'>
-        <h2>Subtotal goes here</h2>
-      </div>
-    </div>
+      <Grid item xs={12} sm={3} className='checkout__right'>
+        <Subtotal />
+      </Grid>
+    </Grid>
   );
 }
 
